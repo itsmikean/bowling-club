e0f71cd5aa704c3668632c91e03b3154d7acf5f6
Mike Adil <itsmikean@gmail.com>
2019-03-20 06:54:35 +1100

    test initial state

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Bowling functions', () => {
+  describe('initial state', () => {
+    it('should have initial state', () => {
+      const app = new App({});
+      expect(app.state.allTries).toEqual([]);
+      expect(app.state.currentTry).toBe(0);
+    });
+  });
 });
