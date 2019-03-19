5be5016642029ed1ecd9c9a10fe1d48c5beb589f
Mike Adil <itsmikean@gmail.com>
2019-03-20 06:41:09 +1100

    cleanup and initial state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTries: [],
+      currentTry: 0,
+    };
+  }
+
   render() {
+    const { allTries, currentTry } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          All tries: {JSON.stringify(allTries)}<br />
+          Current try: {currentTry + 1}<br />
         </header>
       </div>
     );
