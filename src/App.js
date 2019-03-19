d3fec2346aafd8a76d7d1643360b99c9057bca19
Mike Adil <itsmikean@gmail.com>
2019-03-20 07:38:27 +1100

    get score from all tries

--- src/App.js
+++ src/App.js
@@ -19,6 +19,45 @@ class App extends Component {
     })
   }
 
+  getScoreForStrike = (allTries, currentIndex) => {
+    const hasPlayedAfterStrike = typeof allTries[currentIndex + 3] !== 'undefined';
+    if(hasPlayedAfterStrike){
+      return allTries[currentIndex] + allTries[currentIndex + 2] + allTries[currentIndex + 3];
+    }
+    return allTries[currentIndex];
+  }
+
+  getScoreForSpare = (allTries, currentIndex) => {
+    const hasPlayedAfterSpare = typeof allTries[currentIndex + 2] !== 'undefined';
+    if(hasPlayedAfterSpare){
+      return allTries[currentIndex] + allTries[currentIndex + 1] + allTries[currentIndex + 2];
+    }
+    return allTries[currentIndex] + allTries[currentIndex + 1];
+  }
+
+  getScoreForFrame = (frameNumber) => {
+    const currentIndex = frameNumber * 2;
+    const { allTries } = this.state;
+    const isStrike = allTries[currentIndex] === 10;
+    const isSpare = allTries[currentIndex] + allTries[currentIndex + 1] === 10;
+
+    if(isStrike){
+      return this.getScoreForStrike(allTries, currentIndex);
+    } else if(isSpare){
+      return this.getScoreForSpare(allTries, currentIndex);
+    } else {
+      return allTries[currentIndex] + allTries[currentIndex + 1];
+    }
+  }
+  
+  score = () => {
+    let currentScore = 0;
+    for (let currentIndex = 0; currentIndex < 10; currentIndex++) {
+      currentScore = currentScore + this.getScoreForFrame(currentIndex);  
+    }
+    return currentScore;
+  }
+
   render() {
     const { allTries, currentTry } = this.state;
     return (
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import App from './App';
 
 describe('Bowling functions', () => {
-  describe('initial state', () => {
-    it('should have initial state', () => {
+  describe('state', () => {
+    it('should be expected initial state', () => {
+      const expectedInitialState = { allTries: [], currentTry: 0 };
       const app = new App({});
-      expect(app.state.allTries).toEqual([]);
-      expect(app.state.currentTry).toBe(0);
+      expect(app.state).toEqual(expectedInitialState);
     });
   });
 });
